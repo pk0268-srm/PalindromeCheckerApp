1c2ccaf44ed92f2908f4c90c696ba24b6068ea24
pk0268-srm <pk0268@srmist.edu.in>
2026-02-27 10:43:45 +0530

    UC10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,23 @@
 public class PalindromeCheckerApp {
-    public static boolean isPalindrome(String text, int start, int end) {
-        if (start >= end) {
-            return true;
+
+    public static boolean isPalindrome(String text) {
+        text = text.replaceAll("\\s+", "").toLowerCase();
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        if (text.charAt(start) != text.charAt(end)) {
-            return false;
-        }
-        return isPalindrome(text, start + 1, end - 1);
+        return true;
     }
+
     public static void main(String[] args) {
-        String inputText = "level";
-        boolean result = isPalindrome(inputText, 0, inputText.length() - 1);
+        String inputText = "A man a plan a canal Panama";
+        boolean result = isPalindrome(inputText);
 
         System.out.println("Input text: " + inputText);
         System.out.println("Is it a Palindrome? : " + result);
