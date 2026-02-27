dad91ef0a8ac7f602bf82d822f187959f618ef0b
pk0268-srm <pk0268@srmist.edu.in>
2026-02-27 10:00:04 +0530

    UC3:Reversed Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,12 +1,13 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String inputText = "madam";
+        String inputText = "level";
         String reversedText = "";
         for (int i = inputText.length() - 1; i >= 0; i--) {
             reversedText = reversedText + inputText.charAt(i);
         }
         boolean isPalindrome = inputText.equals(reversedText);
         System.out.println("Input text: " + inputText);
+        System.out.println("Reversed text: " + reversedText);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
