d19cfac3cf9425be9601f63890aff3dc7798fc29
pk0268-srm <pk0268@srmist.edu.in>
2026-02-27 09:25:52 +0530

    UC1: Add welcome message module

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+    private static final String APP_NAME = "PalindromeChecker App";
+    private static final String APP_VERSION = "Version 1.0";
+    public static void main(String[] args) {
+        System.out.println("      Welcome to " + APP_NAME);
+        System.out.println("              " + APP_VERSION);
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println("Application initialized successfully.");
+    }
+}
