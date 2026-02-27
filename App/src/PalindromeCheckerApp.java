c7f8394e15738afbb6db4758fe1fb43755663049
pk0268-srm <pk0268@srmist.edu.in>
2026-02-27 10:51:27 +0530

    UC12

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,63 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Stack;
 public class PalindromeCheckerApp {
-
-    public static boolean isPalindrome(String text) {
-        text = text.replaceAll("\\s+", "").toLowerCase();
-        int start = 0;
-        int end = text.length() - 1;
-
-        while (start < end) {
-            if (text.charAt(start) != text.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+    interface PalindromeStrategy {
+        boolean check(String text);
     }
+    static class StackStrategy implements PalindromeStrategy {
+        @Override
+        public boolean check(String text) {
+            Stack<Character> stack = new Stack<>();
+            for (char c : text.toCharArray()) {
+                stack.push(c);
+            }
+            for (char c : text.toCharArray()) {
+                if (c != stack.pop()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    static class DequeStrategy implements PalindromeStrategy {
+        @Override
+        public boolean check(String text) {
+            Deque<Character> deque = new ArrayDeque<>();
+            for (char c : text.toCharArray()) {
+                deque.addLast(c);
+            }
+            while (deque.size() > 1) {
+                if (deque.removeFirst() != deque.removeLast()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    static class PalindromeContext {
+        private PalindromeStrategy strategy;
 
+        public PalindromeContext(PalindromeStrategy strategy) {
+            this.strategy = strategy;
+        }
+
+        public boolean executeStrategy(String text) {
+            return strategy.check(text);
+        }
+    }
     public static void main(String[] args) {
-        String inputText = "A man a plan a canal Panama";
-        boolean result = isPalindrome(inputText);
-
+        String inputText = "madam";
+        PalindromeContext stackContext = new PalindromeContext(new StackStrategy());
+        boolean resultStack = stackContext.executeStrategy(inputText);
+        System.out.println("Using Stack Strategy:");
         System.out.println("Input text: " + inputText);
-        System.out.println("Is it a Palindrome? : " + result);
+        System.out.println("Is it a Palindrome? : " + resultStack);
+
+        PalindromeContext dequeContext = new PalindromeContext(new DequeStrategy());
+        boolean resultDeque = dequeContext.executeStrategy(inputText);
+        System.out.println("Using Deque Strategy:");
+        System.out.println("Input text: " + inputText);
+        System.out.println("Is it a Palindrome? : " + resultDeque);
     }
 }
