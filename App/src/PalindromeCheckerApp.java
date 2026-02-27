626686461cdbdb7621a2a2efed2004a32b651fa4
pk0268-srm <pk0268@srmist.edu.in>
2026-02-27 10:19:29 +0530

    UC6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,21 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String inputText = "madam";
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < inputText.length(); i++) {
-            stack.push(inputText.charAt(i));
+            char ch = inputText.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
         boolean isPalindrome = true;
-        for (int i = 0; i < inputText.length(); i++) {
-            char poppedChar = stack.pop();
-
-            if (inputText.charAt(i) != poppedChar) {
+        while (!queue.isEmpty()) {
+            char fromQueue = queue.remove();   // Dequeue
+            char fromStack = stack.pop();      // Pop
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
